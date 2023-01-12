ac699f66710dc343b84f8f036e2b542a3e5fad9b
KaiNizam <kainizam115@gmail.com>
2023-01-13 03:37:44 +0800

    2
    2

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
 Route::get('/', function () {
-    return view('Home');
+    return view('Manage Committee Election/test');
 });
 
 Route::middleware([
